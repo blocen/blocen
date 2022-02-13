4625d75cb5397ea12d5bfc11a08d224df872bb01
Raphael <89421168+blocen@users.noreply.github.com>
2022-02-13 14:40:12 +0100

    simple collection

--- simple_collection.hs
+++ simple_collection.hs
@@ -8,6 +8,21 @@ main :: IO ()
 main = do
   putStrLn $ s ++ " no. " ++ show x
   putStrLn $ show $ add 3 4
+  print (add 3 4)
+  let t = "test world"
+  print t
+  putStrLn "enter int: "
+  str <- getLine
+  let i = read str :: Int
+  print str
+  print i 
+  putStrLn $ "str is: " ++ show str
+  putStrLn $ "i is: " ++ show (i * 2)
+  let g = "2"
+  case g of
+    "1" -> putStrLn "one"
+    "2" -> putStrLn "tow"
+    _ -> putStrLn "none"
 
 data DateInfo = MyDate Int Int Int 
   deriving (Show, Eq)
@@ -25,5 +40,7 @@ me :: Student
 me = Student "sam" "lom" 46
 
 add :: Int -> Int -> Int
+-- add x y = do
+--   x + y
 add x y = x + y
 
